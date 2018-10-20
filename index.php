e5d40fbb3ae0f36500dae8a7713532b8f2c22aee
Ibnu Pratama <pibnu87@gmail.com>
2018-10-20 17:51:59 +0700

    function

--- index.php
+++ index.php
@@ -2,11 +2,30 @@
 
 require 'function.php';
 
+
 if(isset($_POST["submit"])){
     if(tambah($_POST) > 0){
-        echo 'data berhasil dimasukan';
+
+        echo "<script>
+    
+        swal2(
+                'Good job!',
+                'You clicked the button!',
+                'success'
+        );
+            
+       </script>";
+
     }else{
-        echo'data tidak berhasil ditambahkan';
+        echo "
+        <script>
+            swal({
+                type: 'error',
+                title: 'Oops...',
+                text: 'Something went wrong!',
+                footer: '<a href>Why do I have this issue?</a>'
+            });
+        </script>";
     }
 
 }
@@ -25,11 +44,12 @@ if(isset($_POST["submit"])){
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="Js/sweetalert2.css">
+    <script scr="Js/sweetalert2.min.js"></script>
 
     <title>Klinik Indonesia</title>
   </head>
   <body>
-   
 
    <nav class="icon">
     <a href="index.php"><img src="icon/icon.svg" alt=""></a>
@@ -213,6 +233,7 @@ if(isset($_POST["submit"])){
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
