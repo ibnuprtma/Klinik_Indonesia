c17f3735b10d3f65f13ff35417bea46e400f0160
Ibnu Pratama <pibnu87@gmail.com>
2018-10-20 17:51:22 +0700

    add data

--- function.php
+++ function.php
@@ -15,17 +15,32 @@
     function tambah($dataP){
         global $conn;
 
+                
+        date_default_timezone_set('Asia/Jakarta');
+        $date = date('d/m/Y', time());
+        $digits = 3;
+
+        $kd_pas = rand(pow(10, $digits-1), pow(10, $digits)-1);
         $nm_pas = $dataP["nama"];
         $almt_pas = $dataP["alamat"];
         $jkel_pas = $dataP["rJK"];
         $darah_pas = $dataP["rGD"];
         $lhr_pas = $dataP["Tanggal-Lahir"];
+        $tgl_daftar = $date;
 
         $query = "INSERT INTO PASIEN 
                     VALUES
-                ('','$nm_pas','$almt_pas','$jkel_pas',' $darah_pas',' $lhr_pas','')";
+                (
+                '$kd_pas',
+                '$nm_pas',
+                '$almt_pas',
+                '$jkel_pas',
+                '$darah_pas',
+                '$lhr_pas',
+                '$tgl_daftar'
+                )";
 
-        $result;
+        mysqli_query($conn, $query);
         return mysqli_affected_rows($conn);
     }
 
